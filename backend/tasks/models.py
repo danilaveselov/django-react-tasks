ab90c99a6365efa3ff0ff7e955ac581d73192a63
danilaveselov <lonyagey@gmail.com>
2023-09-10 07:38:34 +0100

    Models created

--- backend/tasks/models.py
+++ backend/tasks/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from datetime import date
 
-# Create your models here.
+
+class Tile(models.Model):
+    class Status(models.IntegerChoices):
+        LIVE = 1
+        PENDING = 2
+        ARCHIVED = 3
+
+    launch_date = models.DateField(default=date.today)
+    status = models.IntegerField(choices=Status.choices)
+
+    def __str__(self):
+        return f'{self.pk} - {self.status}'
+
+
+class Task(models.Model):
+    class Type(models.IntegerChoices):
+        SURVEY = 1
+        DISCUSSION = 2
+        DIARY = 3
+
+    title = models.CharField(max_length=100)
+    order = models.PositiveSmallIntegerField()
+    description = models.TextField()
+    type = models.IntegerField(choices=Type.choices)
+    tile = models.ForeignKey(Tile, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.pk} - {self.title}'
