94fa3915b621a2055c6c92580f1a8bbc2cfe8a0d
danilaveselov <lonyagey@gmail.com>
2023-09-10 07:38:57 +0100

    Displaying models in admin

--- backend/tasks/admin.py
+++ backend/tasks/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Tile, Task
 
-# Register your models here.
+
+class TileModelAdmin(admin.ModelAdmin):
+    list_display = ['launch_date', 'status']
+
+
+class TaskModelAdmin(admin.ModelAdmin):
+    list_display = ['title', 'order', 'description', 'type', 'tile']
+
+
+admin.site.register(Tile, TileModelAdmin)
+admin.site.register(Task, TaskModelAdmin)
