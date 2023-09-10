8fad9ecb750bb0f698bb894cb63e5507eb43d316
danilaveselov <lonyagey@gmail.com>
2023-09-10 07:42:04 +0100

    App js to work with MUI datepicker

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,18 @@
-import logo from './logo.svg';
+import React from "react";
+import TileList from "./components/TileList";
+import Container from "@mui/material/Container";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
+import enGB from 'date-fns/locale/en-GB';
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+    return (
+        <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={enGB}>
+            <Container>
+                <TileList />
+            </Container>
+        </LocalizationProvider>
+    );
 }
 
 export default App;
