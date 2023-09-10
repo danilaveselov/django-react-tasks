d60e0a2a38951a6d39556f0dbb5a35a24dcb3398
danilaveselov <lonyagey@gmail.com>
2023-09-10 14:34:08 +0100

    Task & Tile avatar components

---
+++ frontend/src/components/TaskTypeAvatar.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+// Icons
+import ChatIcon from '@mui/icons-material/Chat';
+import ImportContactsIcon from '@mui/icons-material/ImportContacts';
+import PollIcon from '@mui/icons-material/Poll';
+import QuestionMarkIcon from '@mui/icons-material/QuestionMark';
+// Material UI
+import Avatar from '@mui/material/Avatar';
+import Tooltip from '@mui/material/Tooltip';
+
+const TaskTypeAvatar = ({ type }) => {
+    switch (type) {
+        case 1:
+            return <Tooltip title="Survey"><Avatar><PollIcon /></Avatar></Tooltip>;
+        case 2:
+            return <Tooltip title="Discussion"><Avatar><ChatIcon /></Avatar></Tooltip>;
+        case 3:
+            return <Tooltip title="Diary"><Avatar><ImportContactsIcon /></Avatar></Tooltip>;
+        default:
+            return <Avatar><QuestionMarkIcon /></Avatar>;
+    }
+}
+
+export default TaskTypeAvatar
---
+++ frontend/src/components/TileStatusAvatar.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+// Colours
+import { deepOrange, green, pink } from "@mui/material/colors";
+// Icons
+import AppsIcon from "@mui/icons-material/Apps";
+import ArchiveIcon from "@mui/icons-material/Archive";
+import HistoryIcon from "@mui/icons-material/History";
+import StarIcon from "@mui/icons-material/Star";
+// Material UI
+import Avatar from "@mui/material/Avatar";
+import Tooltip from "@mui/material/Tooltip";
+
+const TileStatusAvatar = ({ status }) => {
+    switch (status) {
+        case 1:
+            return (
+                <Tooltip title="Live">
+                    <Avatar variant="square" sx={{ bgcolor: green[500] }}>
+                        <StarIcon />
+                    </Avatar>
+                </Tooltip>
+            );
+        case 2:
+            return (
+                <Tooltip title="Pending">
+                    <Avatar variant="square" sx={{ bgcolor: pink[500] }}>
+                        <HistoryIcon />
+                    </Avatar>
+                </Tooltip>
+            );
+        case 3:
+            return (
+                <Tooltip title="Archived">
+                    <Avatar variant="square" sx={{ bgcolor: deepOrange[500] }}>
+                        <ArchiveIcon />
+                    </Avatar>
+                </Tooltip>
+            );
+        default:
+            return (
+                <Avatar variant="square" sx={{ bgcolor: green[500] }}>
+                    <AppsIcon />
+                </Avatar>
+            );
+    }
+};
+
+export default TileStatusAvatar;
