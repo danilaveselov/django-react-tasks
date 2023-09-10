c2640f886be47b988e028c98711f47ca6c9910ea
danilaveselov <lonyagey@gmail.com>
2023-09-10 07:39:47 +0100

    Serializers and views setup

--- backend/tasks/views.py
+++ backend/tasks/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from .models import Tile, Task
+from .serializers import TileSerializer, TaskSerializer
 
-# Create your views here.
+
+class TileViewSet(viewsets.ModelViewSet):
+    queryset = Tile.objects.all()
+    serializer_class = TileSerializer
+
+    @action(detail=True, methods=['GET'])
+    def tasks(self, request, pk=None):
+        tile = self.get_object()
+        tasks = Task.objects.filter(tile=tile)
+        serializer = TaskSerializer(tasks, many=True)
+        return Response(serializer.data)
+
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
